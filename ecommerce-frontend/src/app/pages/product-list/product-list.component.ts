d855ff24e7f49434c3bd8fc5529dd34583b41760
Joao Uchoa <joaovictoruchoa01@gmail.com>
2025-12-05 20:14:00 -0300

    feat: implementa listagem de produtos.

---
+++ ecommerce-frontend/src/app/pages/product-list/product-list.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ProductService } from '../../services/products/product.service';
+import { CartService } from '../../services/shopping-cart/cart.service';
+import { Product } from '../../models/product.model';
+import { TableComponent } from '../../components/table/table.component';
+import id from '@angular/common/locales/extra/id';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule, RouterLink, TableComponent],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ListagemProdutosComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  // Carrega produtos 
+  loadProducts(): void {
+    this.productService.findAll().subscribe({
+      next: (data) => {
+        this.products = data;
+      },
+      error: (err) => {
+        console.error('Erro ao carregar produtos:', err);
+        alert('Erro ao carregar produtos. Verifique se o backend está rodando.');
+      }
+    });
+  }
+
+  // Remover item 
+  handleDelete(id: number): void {
+    if (confirm('Deseja realmente excluir este produto?')) {
+      this.productService.delete(id).subscribe({
+        next: () => {
+          // Atualiza a lista 
+          this.products = this.products.filter(p => p.id !== id);
+          alert('Produto excluído com sucesso!');
+        },
+        error: (err) => {
+          console.error('Erro ao excluir produto:', err);
+          alert('Erro ao excluir produto');
+        }
+      });
+    }
+  }
+
+  // Adicionar produto ao carrinho
+  handleAddToCart(product: Product): void {
+    this.cartService.addToCart(product);
+    alert(`${product.name} adicionado ao carrinho!`);
+  }
+}
